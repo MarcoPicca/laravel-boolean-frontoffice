6f6e8e17c9850bc642b57765205a7b944c15ffab
MarcoPicca <il96marco@gmail.com>
2024-02-23 18:48:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+  export default{
+    components: { AppHeader, AppMain }
+}
+</script>
+
+<template>
+    <AppHeader/>
+    <router-view></router-view>
+</template>
+
+<style lang="scss">
+  @use './style/general.scss' as *;
+</style>
